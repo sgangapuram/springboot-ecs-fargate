6866a29a4198f2923a956a2efac76173a6f19737
niraj <niraj.sonawane@gmail.com>
2023-04-09 13:53:41 +0200

    First Commit

---
+++ src/main/java/com/ns/ecsfargate/SpringBootEcsFargateApplication.java
@@ -0,0 +1,13 @@
+package com.ns.ecsfargate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootEcsFargateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootEcsFargateApplication.class, args);
+	}
+
+}
